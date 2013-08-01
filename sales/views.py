dfd4ccdc7ef7a4bb9e7d5a6be7c9f273a4a2f7a8
mehrnooshep <mehrnoosh.ebrahimipour@gmail.com>
2013-08-01 00:40:01 -0700

    fnc push

--- sales/views.py
+++ sales/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from sales.models import SaleBill
+from fnc.models import CostBenefit
 
 
 def index(request):
     #request.get['username']
     #request.post
-    #l = SaleBill.objects.all()[0]
+	l = SaleBill.objects.all()[0]
+	cost_benefit_objects= CostBenefit.objects.all()
+	return render(request, 'sales/index.html', {})
     #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
-    return render(request, 'sales/index.html', {})
+
 
 def index2(request):
     #request.get['username']
