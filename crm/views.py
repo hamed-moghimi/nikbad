db3ca3e7acdbccb24474ca2187f4adaa5dc7f8c4
zurix <zarrin.montazeri@gmail.com>
2013-08-01 15:15:35 +0430

    taghir dar modele feedback

--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import context
 from sales.models import SaleBill
+from crm.models import *
 
 
 def index(request):
@@ -23,11 +25,12 @@ def signUp(request):
     #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
     return render(request, 'crm/signUp.html', {})
 def status(request):
-    #request.get['username']
-    #request.post
-    #l = SaleBill.objects.all()[0]
-    #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
-    return render(request, 'crm/status.html', {})
+    c = Customer.objects.all()[0]
+    s = SaleBill.get().filter(Customer=c)
+    sb = c.saleBills
+    context = { 'Product' : p }
+    return render(request, 'crm/status.html', context)
+
 def success(request):
     #request.get['username']
     #request.post
