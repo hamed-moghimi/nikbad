de29b2f4724cf5da13d277dbf0aa71e6c5f90b97
zurix <zarrin.montazeri@gmail.com>
2013-08-03 13:34:21 +0430

    sabte name karbar ezafe shod , edit mande

--- crm/forms.py
+++ crm/forms.py
@@ -1,9 +1,11 @@
 from django.forms.models import ModelForm
 from crm.models import Customer
+from django.forms import *
 
 class CustomerForm(ModelForm):
-
+    #sword = forms.CharField(widget = forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
     class Meta:
         model = Customer
-        fields = ['username',  'password' , 'first_name', 'last_name', 'gender' , 'email'  , 'postal_code' , 'address' ]
+        fields = ['username',  "Password" , 'first_name', 'last_name', 'gender' , 'phone'  , 'email'  , 'postal_code', 'city' , 'address' ]
 
--- crm/views.py
+++ crm/views.py
@@ -14,26 +14,31 @@ def index(request):
     return render(request, 'crm/base.html', {})
 
 def edit(request):
-    #request.get['username']
-    #request.post
-    #l = SaleBill.objects.all()[0]
-    #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
-    return render(request, 'crm/edit.html', {})
-def signUp(request):
     f = CustomerForm(request.POST)
+    f.changed_data()
+    return render(request, 'crm/edit.html', {'CustomerForm' : f})
+
+def signUp(request):
+    if(request.POST):
+        f = CustomerForm(request.POST)
+        if (f.is_valid()):
+            f.save()
+
+            return success(request)
+    else:
+        f = CustomerForm()
     return render(request, 'crm/signUp.html', {'CustomerForm' : f})
+
 def status(request):
     c = Customer.objects.all()[0]
-    sb = c.saleBills.latest()
-    p =  sb.products
+    sb = c.saleBills.all()[0]
+    p =  sb.products.all()
+    print ("bill ")
+    print sb
     context = { 'Product' : p , 'Bill' : sb }
     return render(request, 'crm/status.html', context)
 
 def success(request):
-    #request.get['username']
-    #request.post
-    #l = SaleBill.objects.all()[0]
-    #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
     return render(request, 'crm/signUp-successful.html', {})
 
 def index2(request):
