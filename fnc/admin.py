c4e8c2c0df02a48c8d529b12177a212628a6d678
mehrnooshep <mehrnoosh.ebrahimipour@gmail.com>
2013-08-03 00:51:53 -0700

    fnc starts!!! :D

---
+++ fnc/admin.py
@@ -0,0 +1,9 @@
+from fnc.models import *
+from django.contrib import admin
+
+admin.site.register(CostBenefit)
+admin.site.register(Employee)
+admin.site.register(RollCall)
+admin.site.register(SalaryFactor)
+admin.site.register(GeneralAccount)
+
