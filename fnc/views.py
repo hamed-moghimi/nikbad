39360008d228c24df85cfe06eaa563a10a6f4675
mehrnooshep <mehrnoosh.ebrahimipour@gmail.com>
2013-08-03 02:20:08 -0700

    fnc starts!!! :D

--- fnc/views.py
+++ fnc/views.py
@@ -21,9 +21,37 @@ def gozaresh_mali(request):
 	cb_objects= CostBenefit.objects.all()
 	context={}
 	context.update({'costBenefits':cb_objects})
+
+	sb_objects=SaleBill.objects.all()
+
+	context.update({'saleBills':sb_objects})
+
 	return render(request, 'fnc/gozaresh_mali.html', context)
 def sabtenam_karmand(request):
+	if(request.POST):
+		form = EmployeeForm(request.POST)
+		if(form.is_valid()):
+			form.save()
+			form_test = ItemForm()
+			context_test={}
+			context_test.update({'emp_form':form_test})
+			return render(request,'fnc/sabtenam_karmand_review.html', context_test)
+	else:
+		form = EmployeeForm()
 	context={}
-	form = EmployeeForm()
 	context.update({'emp_form':form})
 	return render(request, 'fnc/sabtenam_karmand.html', context)
+
+class ItemForm(EmployeeForm):
+    def __init__(self, *args, **kwargs):
+        super(ItemForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            self.fields['sku'].widget.attrs['readonly'] = True
+
+    def clean_sku(self):
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            return instance.sku
+        else:
+            return self.cleaned_data['sku']
