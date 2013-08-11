032825e913eefd6020557855855ea8b2748bae0f
zurix <zarrin.montazeri@gmail.com>
2013-08-11 19:13:29 +0430

    table ok shod

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,11 @@
 #from Demos.win32ts_logoff_disconnected import *
+from django.contrib.auth.decorators import permission_required
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
-from django.http import HttpResponse
-from crm.forms import CustomerForm,EditForm
+from django.http import HttpResponse, HttpResponseForbidden
+from crm.forms import CustomerForm,EditForm, checkCustomerForm
 
 from crm.models import *
-
 
 def index(request):
     print request.user.username
@@ -21,29 +22,31 @@ def index(request):
     return render(request, 'crm/base.html', context)
 
 def edit(request):
-    # f = CustomerForm(request.POST)
     c = Customer.objects.get(username=request.user.username)
-    f = CustomerForm( instance=c )
-
-    # c = Customer.objects.all()[0]
-    sb = c.saleBills.all()[0]
-    p =  sb.products.all()
-    context = { 'Product' : p , 'Bill' : sb }
-    return render(request, 'crm/edit.html', {'CustomerForm' : f})
+    f = EditForm( instance=c )
+    if(request.POST):
+        if (f.is_valid()):
+             f.save()
+             print " khhhhhhhhhhhhhhhhhhhhhhhhh"
+             return edit_success(request)
+    else :
+        print "intoooooooooooooooo"
+        return render(request, 'crm/edit.html', {'EditForm' : f})
 
 def signUp(request):
     if(request.POST):
         f = CustomerForm(request.POST)
         if (f.is_valid()):
+            f.instance.set_password(f.cleaned_data['password'])
             f.save()
             return success(request)
     else:
         f = CustomerForm()
     return render(request, 'crm/signUp.html', {'CustomerForm' : f})
 
-
+@permission_required('crm.is_customer', login_url=reverse_lazy('sales-index'))
 def status(request):
-    c = Customer.objects.all()[0]
+    c = Customer.objects.get(username=request.user.username)
     sb = c.saleBills.all()[0]
     p =  sb.products.all()
 
@@ -51,22 +54,25 @@ def status(request):
     return render(request, 'crm/status.html', context)
 
 def success(request):
-    f = CustomerForm(request.POST)
-    return render(request, 'crm/signUp-successful.html', {'CustomerForm' : f})
+    f = checkCustomerForm(request.POST)
+    return render(request, 'crm/signUp-successful.html', {'checkCustomerForm' : f})
 
-def index2(request):
-    #request.get['username']
-    #request.post
-    #l = SaleBill.objects.all()[0]
-    #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
-    return render(request, 'base.html', {})
+def edit_success(request):
+    f = EditForm(request.POST)
+    return render(request, 'crm/edit-successful.html', {'EditForm' : f})
 
-
+@permission_required('crm.is_customer', login_url=reverse_lazy('sales-index'))
 def feedback(request) :
-    f = Feedback()
-    f.content = request.POST.content
-    c = Customer.objects.get(username=request.user.username)
-    f.Customer = c
-    f.product = request.POST.productID
-    f.save()
-    return HttpResponse("ok")
+    try:
+        if(request.POST and request.is_ajax()):
+            f = Feedback()
+            f.content = request.POST['content']
+            c = Customer.objects.get(username=request.user.username)
+            f.Customer = c
+            f.product = request.POST['productID']
+            f.save()
+            return HttpResponse("ok")
+        else:
+            return HttpResponseForbidden()
+    except:
+        return HttpResponseForbidden()
