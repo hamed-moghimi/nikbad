45f2a93d97a020273fbf818de541a5920a8e11c7
rjavadi <roya.javadi26@gmail.com>
2013-08-12 17:33:02 +0430

    mng contract added

--- mng/urls.py
+++ mng/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^manager$', 'mng.views.index', name='manager'),
+    url(r'^contract$', 'mng.views.newContract', name='contract'),
 
 )
