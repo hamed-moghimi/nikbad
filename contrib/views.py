c8f1e2f71d0f3183e09688bc22ac6c5fc23f2417
pourreza <mm.pourreza@gmail.com>
2013-08-13 04:12:15 +0430

    warehouse_ taghriban taheshe :D

--- contrib/views.py
+++ contrib/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 def login(request):
     print('login')
     if request.method == 'POST':
-        username = request.POST['username']
+        username    = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(username = username, password = password)
         if user is not None and user.is_active:
@@ -16,6 +16,12 @@ def login(request):
             if user.has_perm('crm.is_customer'):
                 # Customers
                 return HttpResponseRedirect(reverse('sales-index'))
+            elif user.has_perm('warehouse.is_warehouseman') and user.has_perm('warehouse.is_mng_warehouse'):
+                # Warehouseman ke moshtarak ba manager ham dare
+                return HttpResponseRedirect(reverse('warehouse-index'))
+            elif user.has_perm('warehouse.is_deliveryman'):
+                # Deliveryman
+                return HttpResponseRedirect(reverse('warehouse-index'))
             elif user.has_perm('wiki.is_wiki'):
                 # Wikis
                 return HttpResponseRedirect(reverse('wiki-index'))
