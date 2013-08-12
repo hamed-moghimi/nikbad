c8b777e3c396072a12f9e1113cf50d41c81190fb
zurix <zarrin.montazeri@gmail.com>
2013-08-12 12:52:52 +0430

    manger add shod

--- mng/urls.py
+++ mng/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^manager$', 'mng.views.index', name='manager'),
+    url(r'^manager$', 'mng.views.base', name='manager'),
+    url(r'^manager-fnc$', 'mng.views.fnc', name='mng-fnc'),
+    url(r'^manager-wiki$', 'mng.views.wiki', name='mng-wiki'),
+    url(r'^manager-sales$', 'mng.views.sales', name='mng-sales'),
+    url(r'^manager-wrh$', 'mng.views.wrh', name='mng-wrh'),
+    url(r'^manager-emp$', 'mng.views.emp', name='mng-emp'),
 
-)
+
+    )
--- mng/views.py
+++ mng/views.py
@@ -6,67 +6,27 @@ from crm.forms import CustomerForm,EditForm
 from crm.models import *
 
 
-def index(request):
-    print request.user.username
-    c = Customer.objects.get(username=request.user.username)
-    print("here  sdsd"+ c.username)
-    f = EditForm( instance=c )
-    # if (f.is_valid()):
-    # f.save()
-    print "saveeeeeeeeeeeeeeeeed"
-    sb = c.saleBills.all()[0]
-    p =  sb.products.all()
-    context = { 'Product' : p , 'Bill' : sb , 'EditForm' : f}
+def base(request):
 
-    return render(request, 'crm/base.html', context)
+    return render(request, 'mng/base.html', {})
 
-def edit(request):
-    # f = CustomerForm(request.POST)
-    c = Customer.objects.get(username=request.user.username)
-    f = CustomerForm( instance=c )
+def emp(request):
 
-    # c = Customer.objects.all()[0]
-    sb = c.saleBills.all()[0]
-    p =  sb.products.all()
-    context = { 'Product' : p , 'Bill' : sb }
-    return render(request, 'crm/edit.html', {'CustomerForm' : f})
+    return render(request, 'mng/mng-emp.html', {})
 
-def signUp(request):
-    if(request.POST):
-        f = CustomerForm(request.POST)
-        if (f.is_valid()):
-            f.save()
-            return success(request)
-    else:
-        f = CustomerForm()
-    return render(request, 'crm/signUp.html', {'CustomerForm' : f})
+def wrh(request):
+
+    return render(request, 'mng/mng-wrh.html', {})
 
 
-def status(request):
-    c = Customer.objects.all()[0]
-    sb = c.saleBills.all()[0]
-    p =  sb.products.all()
+def fnc(request):
 
-    context = { 'Product' : p , 'Bill' : sb }
-    return render(request, 'crm/status.html', context)
+    return render(request, 'mng/mng-fnc.html', {})
 
-def success(request):
-    f = CustomerForm(request.POST)
-    return render(request, 'crm/signUp-successful.html', {'CustomerForm' : f})
+def sales(request):
 
-def index2(request):
-    #request.get['username']
-    #request.post
-    #l = SaleBill.objects.all()[0]
-    #return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
-    return render(request, 'base.html', {})
+    return render(request, 'mng/mng-sales.html', {})
 
+def wiki(request):
 
-def feedback(request) :
-    f = Feedback()
-    f.content = request.POST.content
-    c = Customer.objects.get(username=request.user.username)
-    f.Customer = c
-    f.product = request.POST.productID
-    f.save()
-    return HttpResponse("ok")
+    return render(request, 'mng/mng-wiki.html', {})
