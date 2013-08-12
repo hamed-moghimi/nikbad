3f4e627276f35abea36dc9d89dc3c167ee2400a6
zurix <zarrin.montazeri@gmail.com>
2013-08-12 16:28:42 +0430

    manger add shod

--- mng/urls.py
+++ mng/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^manager$', 'mng.views.base', name='manager'),
-    url(r'^manager-fnc$', 'mng.views.fnc', name='mng-fnc'),
-    url(r'^manager-wiki$', 'mng.views.wiki', name='mng-wiki'),
+    url(r'^manager-fnc$', 'fnc.views.gozaresh_mali', name='mng-fnc'),
+    url(r'^manager-wiki-select$', 'mng.views.select_wiki', name='mng-select-wiki'),
+    url(r'^manager-wiki/(?P<wId>\d+)$', 'mng.views.wiki', name='mng-wiki'),
     url(r'^manager-sales$', 'mng.views.sales', name='mng-sales'),
     url(r'^manager-wrh$', 'mng.views.wrh', name='mng-wrh'),
-    url(r'^manager-emp$', 'mng.views.emp', name='mng-emp'),
-
+    url(r'^manager-newemp$', 'fnc.views.sabtenam_karmand', name='mng-newemp'),
+    url(r'^manager-emp$', 'fnc.views.karmandan', name='mng-emp'),
+    url(r'^manager-emp/(?P<epId>\d+)$', 'fnc.views.karmand_detail', name='mng-emp-detail'),
 
     )
--- mng/views.py
+++ mng/views.py
@@ -2,17 +2,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from crm.forms import CustomerForm,EditForm
-
+from datetime import *
 from crm.models import *
-
+from sales.models import *
+from fnc.models import *
+from wiki.models import *
 
 def base(request):
 
     return render(request, 'mng/base.html', {})
 
-def emp(request):
 
-    return render(request, 'mng/mng-emp.html', {})
+def emp_detail(request , epId):
+    employee=Employee.objects.get(id=epId)
+    rollcalls=RollCall.objects.filter(employee=epId)
+    context = {}
+    context.update({'rollcalls': rollcalls, 'employee': employee })
+    return render(request, 'mng/mng-emp-detail.html', context)
+
+def emp(request):
+    employees = Employee.objects.all()
+    for ep in employees:
+        rollcalls= RollCall.objects.filter(employee=ep)
+        ep.hours=timedelta()
+        for rc in rollcalls:
+            enter= datetime.combine(rc.date,rc.entrance_time)
+            exit = datetime.combine(rc.date,rc.exit_time)
+            ep.hours+=enter-exit
+    context = {}
+    context.update({'employees': employees})
+    return render(request, 'mng/mng-emp.html', context)
 
 def wrh(request):
 
@@ -20,13 +39,26 @@ def wrh(request):
 
 
 def fnc(request):
-
-    return render(request, 'mng/mng-fnc.html', {})
+    cb_objects = CostBenefit.objects.all()
+    context = {}
+    context.update({'costBenefits': cb_objects})
+    sb_objects = SaleBill.objects.all()
+    context.update({'saleBills': sb_objects})
+    return render(request, 'mng/mng-fnc.html', context)
 
 def sales(request):
 
     return render(request, 'mng/mng-sales.html', {})
 
-def wiki(request):
+def select_wiki(request):
+    wikies = Wiki.objects.all()
+    context = {'wikies':wikies}
+    return render(request, 'mng/select-wiki.html', context)
 
-    return render(request, 'mng/mng-wiki.html', {})
+def wiki(request , wId):
+    # if request.user.is_authenticated():
+    #     user = request.user
+    #     myName = user.username
+    p = Product.objects.all().filter( wiki__id = wId)
+    context = {'product_list': p}
+    return render(request, 'mng/mng-wiki.html', context)
