3339737ba41f69024e552f63c42e75d94a8d5005
pourreza <mm.pourreza@gmail.com>
2013-08-13 19:28:47 +0430

    warehouse_ shayad_final

--- contrib/urls.py
+++ contrib/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-        url(r'^$', 'contrib.views.bank', name='contrib-bank'),
+        url(r'^bank$', 'contrib.views.bank', name='contrib-bank'),
 )
