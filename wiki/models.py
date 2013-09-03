b1e700264de01c1d295c9f274517a5d2aa6a2304
rjavadi <roya.javadi26@gmail.com>
2013-09-03 14:56:49 +0430

    :)

--- wiki/models.py
+++ wiki/models.py
@@ -12,7 +12,7 @@ deliveryChoices = (
 # Create your models here.
 class Wiki(User):
     companyName = models.CharField("نام شرکت", max_length = 255)
-    phone = models.SlugField("شماره تلفن", max_length = 11)
+    phone = models.CharField("شماره تلفن", max_length = 11)
     address = models.CharField("نشانی", max_length = 1000)
     image = models.ImageField("تصویر نشان تجاری", blank = True, upload_to = 'images/wikis')
     reminder = models.IntegerField(verbose_name = "باقی مانده حقوق", default = 0)
@@ -36,23 +36,22 @@ class Category(models.Model):
         return self.name
 
 
-class ConRequest(models.Model):
-    wiki = models.ForeignKey(Wiki, verbose_name = u"نام ویکی")
-    pub_date = models.DateField(verbose_name = u"تاریخ ثبت درخواست ایجاد قرارداد")
-    abone = models.IntegerField(verbose_name = u"آبونمان پیشنهادی سالانه")
-    benefit = models.IntegerField(verbose_name = u"درصد بهره پیشنهادی سراب")
+# class ConRequest(models.Model):
+#     wiki = models.ForeignKey(Wiki, verbose_name = u"نام ویکی")
+#     pub_date = models.DateField(verbose_name = u"تاریخ ثبت درخواست ایجاد قرارداد")
+#     abone = models.IntegerField(verbose_name = u"آبونمان پیشنهادی سالانه")
+#     benefit = models.IntegerField(verbose_name = u"درصد بهره پیشنهادی سراب")
 
 
-class ConCancel(models.Model):
-    wiki = models.ForeignKey(Wiki, verbose_name = u"نام ویکی")
-    pub_date = models.DateField(verbose_name = u"تاریخ ثبت درخواست لغو قرارداد")
+# class ConCancel(models.Model):
+#     wiki = models.ForeignKey(Wiki, verbose_name = u"نام ویکی")
+#     pub_date = models.DateField(verbose_name = u"تاریخ ثبت درخواست لغو قرارداد")
 
 
 class SubCat(models.Model):
     name = models.CharField(max_length = 50, verbose_name = u'عنوان')
     # Some other properties here, hazineye anbardari and ... :D
     category = models.ForeignKey('Category', verbose_name = u'دسته', related_name = 'subCats')
-    unit = models.CharField(verbose_name = u'واحد شمارش', default = u'عدد', max_length = 50)
 
     class Meta:
         verbose_name = u'نوع کالا'
@@ -61,17 +60,23 @@ class SubCat(models.Model):
     def __unicode__(self):
         return u'{0} ({1})'.format(self.name, self.category.name)
 
+class Brand(models.Model):
+    name = models.CharField("نام تجاری", max_length=60)
+
+    class Meta:
+        verbose_name = u'نام تجاری'
+        verbose_name_plural = u'نام های تجاری'
 
 class Product(models.Model):
     goodsID = models.IntegerField("کد کالا", primary_key = True)
     wiki = models.ForeignKey(Wiki, verbose_name = "نام ویکی")
-    brand = models.CharField("نام تجاری", max_length = 255)
+    brand = models.ForeignKey(Brand, verbose_name="نام تجاری")
     name = models.CharField("نام کالا", max_length = 255)
-    sub_category = models.ForeignKey(SubCat, verbose_name = "زیر دسته")
+    sub_category = models.ForeignKey(SubCat, verbose_name = "زیر دسته", on_delete=models.PROTECT)
     unit = models.CharField("واحد شمارش", default = "عدد", max_length = 30)
     price = models.IntegerField("قیمت", help_text = "قیمت را به ریال وارد نمایید")
-    off = models.PositiveSmallIntegerField("تخفیف", blank = True, null = True,
-                                           help_text = "تخفیف یک عدد مثبت دو رقمی است")
+    off = models.PositiveSmallIntegerField("تخفیف", default=0,
+                                           help_text = "تخفیف یک عدد مثبت کوچکتر از 100 است")
     deliveryStatus = models.IntegerField(default = 0, choices = deliveryChoices, verbose_name = u'وضعیت تحویل')
     orderPoint = models.PositiveIntegerField(default = 0, verbose_name = u'نقطه سفارش')
 
@@ -79,7 +84,11 @@ class Product(models.Model):
     def __unicode__(self):
         return self.name
 
-    def rawPrice(self):
+    class Meta:
+        verbose_name = u'کالا'
+        verbose_name = u'کالاها'
+
+    def raw_price(self):
         return self.price / (1 - self.off / 100.0)
 
     def isInSale(self):
@@ -94,6 +103,10 @@ class Contract(models.Model):
     percent = models.PositiveSmallIntegerField("درصد بهره ی سراب از فروش")
     fee = models.PositiveIntegerField("آبونمان")
 
+    class Meta:
+        verbose_name = u'قرارداد'
+        verbose_name_plural = u'قراردادها'
+
 
 class ReturnRequest(models.Model):
     wiki = models.ForeignKey(Wiki, verbose_name = u'نام ویکی')
@@ -101,6 +114,11 @@ class ReturnRequest(models.Model):
     pub_date = models.DateField("تاریخ درخواست")
     deliveryStatus = models.IntegerField(default = 0, choices = deliveryChoices, verbose_name = u'وضعیت تحویل')
     returned_only = models.NullBooleanField(verbose_name = u'فقط کالاهای مرجوعی را بازگردان.')
+
+    class Meta:
+        verbose_name = u'درخواست بازگشت کالا از انبار'
+        verbose_name_plural = u'درخواست های بازگشت کالا از انبار'
+
 
 
 class ConRequest(models.Model):
